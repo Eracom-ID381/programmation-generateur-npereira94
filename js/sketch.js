8e08c8393d3a6cc0df787422cd531028d0e5fb5c
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-05-06 16:27:03 +0200

    random rect

--- js/sketch.js
+++ js/sketch.js
@@ -13,25 +13,29 @@ function draw() {
   // et si le timer est écoulé
   if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
     // Appel de notre fonction generator
-    generator(width / 2, height / 2, height - 150);
+    /*generator(width / 2, height / 2, height - 150);*/
   }
-  tronc(width / 2, height / 2, 10, 90);
+  tronc(width / 2, height / 2, 10);
 }
 
-function tronc(x, y, longueur, largeur) {
-  fill(230);
-  rect(x, y, 50, 200);
+function tronc(x, y, size) {
+  for (let offset = size; offset > 0; offset -= 10) {
+    noStroke();
+    fill(random(0, 250), (0, 200), (0, 10));
+    rect(x, y, 50, 400);
+
+  }
 }
 // Fonction avec quatres paramètres
-function generator(x, y, size) {
+/*function generator(x, y, size) {
   // fill utilise le paramètre de notre fonction
   for (let offset = size; offset > 0; offset -= 10) {
     fill(random(0, 250), (0, 200), (0, 10));
-    rect(x, y, 100, offset);
+    rect(x, y, offset, 300);
     console.log(offset);
   }
 }
-
+*/
 function mousePressed() {
   // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
   save();
