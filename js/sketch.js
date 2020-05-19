fcde3820eaa4f595277f3966dbe4cfbb3b8a1981
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-05-19 16:28:20 +0200

    My random palm generator

--- js/sketch.js
+++ js/sketch.js
@@ -15,7 +15,7 @@ function draw() {
   if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
     // Appel de notre fonction generator
     background(255);
-    generator(random(0, 200), height - 150, random(0, 255), random(0, windowWidth));
+    generator(random(0, 200), height - 150, random(0, 255), random(0, 500), random(1000, windowWidth), random(300, 800));
   }
 
 
@@ -23,29 +23,34 @@ function draw() {
 
 
 // Fonction avec quatres paramètres
-function generator(x, y, couleur, position) {
+function generator(x, y, couleur, leafleft, leafright, tronc) {
   // fill utilise le paramètre de notre fonction
 
   //tronc
   noStroke();
   fill(couleur, 42, 42);
-  triangle(width / 2, 300, 700, 800, 780, 800);
+  triangle(width / 2, 300, tronc, 800, 780, tronc);
   //coco
   noStroke();
   fill(120, 42, 42);
-  ellipse(800, position, x, x);
+  ellipse(800, y, x, x);
   ellipse(850, 380, x, x);
   //leafs
   fill(0, couleur, 0);
   strokeWeight(2.2);
   stroke(couleur, couleur, couleur);
-  triangle(width / 2, 300, 200, 300, 380, 200);
-  triangle(width / 2, 300, 400, 600, 500, 400);
-  triangle(width / 2, 300, 1200, 600, 1100, 400);
-  triangle(width / 2, 300, 1200, 300, 1300, 200);
+  triangle(width / 2, 300, 250, 0, leafleft, 100);
+  triangle(width / 2, 300, 200, 300, leafleft, 200);
+  triangle(width / 2, 300, 400, 600, leafleft, 400);
+  triangle(width / 2, 300, 1200, 600, leafright, 400);
+  triangle(width / 2, 300, 1200, 300, leafright, x);
+  triangle(width / 2, 300, 1000, x, leafright, x);
 
-
-
+  //diane cube
+  /*rect(width / 2, height / 2, cube, cube)
+  triangle(width / 2 - cube, height / 2 - cube, width / 2 + cube, height / 2 - cube, width / 2 + 40, height / 2 - 40);
+  triangle(width / 2 - 25, height / 2 - 25, width / 2 - 5, height / 2 - 40, width / 2 + 40, height / 2 - 40);
+*/
 
 
 }
