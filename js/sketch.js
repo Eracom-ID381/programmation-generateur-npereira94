251e56341b4233cc46b089091e299e5be83d8800
npereira94 <60931187+npereira94@users.noreply.github.com>
2020-05-26 17:53:45 +0200

    Constructor palm

--- js/sketch.js
+++ js/sketch.js
@@ -2,83 +2,75 @@
 
 // une unité = 1 seconde
 let timer = 1;
-let tronc;
-let leaf;
-let coco;
+let palmier;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   rectMode(CENTER);
-  fill(20, 30, 40)
-  tronc = new Tronc(width / 2, 300, 300, 800, 780, 400);
-  leaf1 = new Leaf();
-  leaf2 = new Leaf();
-  leaf3 = new Leaf();
-  leaf4 = new Leaf();
-  leaf5 = new Leaf();
-  leaf6 = new Leaf();
-  coco1 = new Coco(800, 300, 20, 20);
-  coco2 = new Coco(850, 380, 100, 100);
+  palmier = new Palmier(random(0, 200), height - 150, random(0, 255), random(0, 500), random(1000, windowWidth), random(300, 800));
+
 }
 
 function draw() {
-  background(0);
-  tronc.afficher();
+
+
   //Conditionellle nous permettant de savoir si la souris est appuyée
   // et si le timer est écoulé
-  //if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
-  // Appel de notre fonction generator
+  if (frameCount % (timer * 60) == 0 && !mouseIsPressed) {
+    // Appel de notre fonction generator
+    palmier.afficher();
+    //constructor(random(0, 200), height - 150, random(0, 255), random(0, 500), random(1000, windowWidth), random(300, 800));
+    //}
+  }
 
-  //constructor(random(0, 200), height - 150, random(0, 255), random(0, 500), random(1000, windowWidth), random(300, 800));
-  //}
+  //Fonction avec quatres paramètres
+  //function generator(x, y, couleur, leafleft, leafright, tronc) {
+  // fill utilise le paramètre de notre fonction
+  /*
+
+  */
 }
-
-
-// Fonction avec quatres paramètres
-//function generator(x, y, couleur, leafleft, leafright, tronc) {
-// fill utilise le paramètre de notre fonction
-/*
-//tronc
-noStroke();
-fill(couleur, 42, 42);
-//triangle(width / 2, 300, tronc, 800, 780, tronc);
-//coco
-noStroke();
-fill(120, 42, 42);
-ellipse(800, y, x, x);
-ellipse(850, 380, x, x);
-//leafs
-fill(0, couleur, 0);
-strokeWeight(2.2);
-stroke(couleur, couleur, couleur);
-triangle(width / 2, 300, 250, 0, leafleft, 100);
-triangle(width / 2, 300, 200, 300, leafleft, 200);
-triangle(width / 2, 300, 400, 600, leafleft, 400);
-triangle(width / 2, 300, 1200, 600, leafright, 400);
-triangle(width / 2, 300, 1200, 300, leafright, x);
-triangle(width / 2, 300, 1000, x, leafright, x);
-
-
-}
-
 
 function mousePressed() {
-  // Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
+  //Fonction de librairie P5 nous permettant de sauvegarder une image du canvas
   save();
 }
-*/
+
 function windowResized() {
   resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
-class Tronc {
-  constructor(x1, y1) {
-    this.x = 100;
-    this.y = 100;
+class Palmier {
+  constructor(x, y, couleur, leafleft, leafright, tronc) {
+    this.x = x;
+    this.y = y;
+    this.couleur = couleur;
+    this.leafleft = leafleft;
+    this.leafright = leafright;
+    this.tronc = tronc;
 
   }
 
   afficher() {
-    triangle(this.x, this.y, 300, 300, 800, 780, 400);
+    //tronc
+    noStroke();
+    fill(this.couleur, 42, 42);
+    triangle(width / 2, 300, this.tronc, 800, 780, this.tronc);
+    //coco
+    noStroke();
+    fill(120, 42, 42);
+    ellipse(800, this.y, this.x, this.x);
+    ellipse(850, 380, this.x, this.x);
+    //leafs
+    fill(0, this.couleur, 0);
+    strokeWeight(2.2);
+    stroke(this.couleur, this.couleur, this.couleur);
+    triangle(width / 2, 300, 250, 0, this.leafleft, 100);
+    triangle(width / 2, 300, 200, 300, this.leafleft, 200);
+    triangle(width / 2, 300, 400, 600, this.leafleft, 400);
+    triangle(width / 2, 300, 1200, 600, this.leafright, 400);
+    triangle(width / 2, 300, 1200, 300, this.leafright, this.x);
+    triangle(width / 2, 300, 1000, this.x, this.leafright, this.x);
 
   }
+}
